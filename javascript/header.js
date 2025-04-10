7b9c47e7f399020efc0b5ea5e17a0160b43bd832
Ezekiel Mokaya Ogana <ezekielmokayaogana>
2025-04-10 17:27:59 +0300

    slideshow

--- javascript/header.js
+++ javascript/header.js
@@ -12,3 +12,49 @@ mainBody.addEventListener("click", (e) => {
         sidenav.classList.toggle("displayNone");
     }
 });
+
+
+
+// slideshow
+let slide1 = document.querySelector(".slide1");
+let slide2 = document.querySelector(".slide2");
+
+//active slides
+let slide1Active = document.querySelector(".slide1Active");
+let slide2Active = document.querySelector(".slide2Active");
+
+//btns
+let slideBtnLeft = document.querySelector(".slideBtnLeft");
+let slideBtnRight = document.querySelector(".slideBtnRight");
+
+setInterval(() => {
+    slide1.classList.toggle("displayNone");
+    slide2.classList.toggle("displayNone");
+
+    if (!slide1.classList.contains("displayNone")) {
+        document.querySelector(".randomTop-text").innerHTML =
+          "Feels good to upgrade";
+    }
+    else {
+        document.querySelector(".randomTop-text").innerHTML =
+          "Take a moment to Rock out!";
+    }
+
+    slide1Active.classList.toggle("activeSlide");
+    slide2Active.classList.toggle("activeSlide");
+}, 5000);
+
+slideBtnLeft.addEventListener("click", (e) => {
+    slide1.classList.toggle("displayNone");
+    slide2.classList.toggle("displayNone");
+
+    slide1Active.classList.toggle("activeSlide");
+    slide2Active.classList.toggle("activeSlide");
+});
+slideBtnRight.addEventListener("click", (e) => {
+    slide1.classList.toggle("displayNone");
+    slide2.classList.toggle("displayNone");
+
+    slide1Active.classList.toggle("activeSlide");
+    slide2Active.classList.toggle("activeSlide");
+});
