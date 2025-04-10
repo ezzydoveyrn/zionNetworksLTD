422a08c6d1fed0082625e0cd252d0e169e654950
Ezekiel Mokaya Ogana <ezekielmokayaogana>
2025-04-10 23:27:46 +0300

    app

--- javascript/header.js
+++ javascript/header.js
@@ -58,3 +58,42 @@ slideBtnRight.addEventListener("click", (e) => {
     slide1Active.classList.toggle("activeSlide");
     slide2Active.classList.toggle("activeSlide");
 });
+
+
+
+
+//app installation
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch((err) => console.error("SW registration failed:", err));
+}
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent the default mini-infobar
+  e.preventDefault();
+  // Stash the event so it can be triggered later
+  deferredPrompt = e;
+
+  // Show your custom button
+  const installBtn = document.querySelector("#installBtn");
+  installBtn.style.display = "block";
+
+  installBtn.addEventListener("click", () => {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted A2HS prompt");
+      } else {
+        console.log("User dismissed A2HS prompt");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
