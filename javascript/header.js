5d9ec78996c1acf532eec45c7bceff8d324aca7f
Ezekiel Mokaya Ogana <ezekielmokayaogana>
2025-04-10 11:13:30 +0300

    interractivity

--- javascript/header.js
+++ javascript/header.js
@@ -3,3 +3,12 @@ let sidenav = document.querySelector(".sidenav-container");
 sidenavBtn.addEventListener('click', (e) => {
     sidenav.classList.toggle('displayNone');
 });
+
+const mainBody = document.querySelector("main");
+mainBody.addEventListener("click", (e) => {
+    if(sidenav.classList.contains("displayNone")){
+        sidenav.classList.add("displayNone");
+    }else if (sidenav.classList !== "displayNone"){
+        sidenav.classList.toggle("displayNone");
+    }
+});
